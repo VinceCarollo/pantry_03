2d7910097d07b96639a862ef68ef1ab4bf9800ca
Vince Carollo <vincecarollo@gmail.com>
2019-04-10 10:33:37 -0600

    Add recipe class and tests

---
+++ test/recipe_test.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+  attr_reader :stir_fry, :ginger
+
+  def setup
+    @stir_fry = Recipe.new('stir fry')
+  end
+
+  def test_it_exists
+    assert_instance_of Recipe, stir_fry
+  end
+
+  def test_it_has_attributes
+    assert_equal 'stir fry', stir_fry.name
+  end
+
+  def test_it_can_add_ingredients
+    ginger = Ingredient.new('ginger', 'gram', 25)
+    bok_choy = Ingredient.new('bok choy', 'gram', 15)
+    stir_fry.add_ingredient(ginger, 1)
+    stir_fry.add_ingredient(bok_choy, 2)
+
+    assert_equal [{1 => ginger}, {2 => bok_choy}], stir_fry.ingredients
+  end
+
+  def test_it_can_check_qty_needed
+    ginger = Ingredient.new('ginger', 'gram', 25)
+    bok_choy = Ingredient.new('bok choy', 'gram', 15)
+    stir_fry.add_ingredient(ginger, 1)
+    stir_fry.add_ingredient(bok_choy, 2)
+
+    assert_equal 2, stir_fry.check_qty(bok_choy)
+  end
+
+  def test_it_can_get_total_calories
+    ginger = Ingredient.new('ginger', 'gram', 25)
+    bok_choy = Ingredient.new('bok choy', 'gram', 15)
+    stir_fry.add_ingredient(ginger, 1)
+    stir_fry.add_ingredient(bok_choy, 2)
+
+    assert_equal 40, stir_fry.total_ingredients
+  end
+end
